966f71188758d63c67ddc9eaf4598f6cb2bcf548
Natanaelvich <taelima1997@gmail.com>
2023-05-24 22:37:30 -0300

    add initial detch historic

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,58 @@
+import { useCallback, useEffect, useState } from "react";
+import { HistoricCard, HistoricCardProps } from "../../components/HistoricCard";
 import { HomeHeader } from "../../components/HomeHeader";
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
 
-import { Container, Content, Title } from "./styles";
+import { Container, Content, Label, Title } from "./styles";
+import { Alert, FlatList } from "react-native";
 
 export function Home() {
+  const [vehicleHistoric, setVehicleHistoric] = useState<HistoricCardProps[]>(
+    []
+  );
+
+  const historic = useQuery(Historic);
+
+  const fetchHistoric = useCallback(() => {
+    try {
+      const response = historic.filtered(
+        "status='arrival' SORT(created_at DESC)"
+      );
+      const formattedHistoric = response.map((item) => {
+        return {
+          id: item._id.toString(),
+          licensePlate: item.license_plate,
+          isSync: false,
+          created: item.created_at.toString(),
+        };
+      });
+      setVehicleHistoric(formattedHistoric);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Histórico", "Não foi possível carregar o histórico.");
+    }
+  }, [historic]);
+
+  useEffect(() => {
+    fetchHistoric();
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
 
       <Content>
         <Title>Histórico</Title>
+
+        <FlatList
+          data={vehicleHistoric}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <HistoricCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={<Label>Nenhum registro de utilização.</Label>}
+        />
       </Content>
     </Container>
   );
