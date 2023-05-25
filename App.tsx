0b2d678ff669c534b516a6c8a5c0574cb43f7df7
Natanaelvich <taelima1997@gmail.com>
2023-05-25 19:23:42 -0300

    update home with user context and fallback page signin

--- App.tsx
+++ App.tsx
@@ -4,20 +4,28 @@ import { Home } from "./src/screens/Home";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
 import { RealmProvider } from "./src/libs/realm";
+import { AppProvider, UserProvider } from "@realm/react";
+import { SignIn } from "./src/screens/SignIn";
+
+import { REALM_APP_ID } from "@env";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <RealmProvider>
-          <Home />
-        </RealmProvider>
-      </SafeAreaProvider>
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <RealmProvider>
+              <Home />
+            </RealmProvider>
+          </UserProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
