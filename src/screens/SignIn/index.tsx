a0d8997515d6166e457c6558cdbaedbcd77093e4
Natanaelvich <taelima1997@gmail.com>
2023-05-25 19:40:21 -0300

    add implementation for google auth

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,20 +1,63 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 import { useApp } from "@realm/react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
 import { Container, Title, Slogan } from "./styles";
 
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { Alert } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
+
 export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const [_, response, googleSignIng] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
   const app = useApp();
 
-  function handleGoogleSignIn() {
+  function handleGoogleSignIn(){
     setIsAuthenticating(true);
+
+    googleSignIng().then(response => {
+      if(response?.type !== 'success') {
+        setIsAuthenticating(false);
+      }
+    })
   }
+  
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        const credentials = Realm.Credentials.jwt(
+          response.authentication.idToken
+        );
+
+        app.logIn(credentials).catch((error) => {
+          console.log(error);
+          Alert.alert(
+            "Entrar",
+            "Não foi possível conectar-se a sua conta google."
+          );
+          setIsAuthenticating(false);
+        });
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
 
   return (
     <Container source={backgroundImg}>
