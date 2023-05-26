c3cecb31559487a9df289267919a30d4f5421829
Natanaelvich <taelima1997@gmail.com>
2023-05-26 18:00:22 -0300

    add routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Home } from "../screens/Home";
+import { Departure } from "../screens/Departure";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      <Screen name="departure" component={Departure} />
+    </Navigator>
+  );
+}
