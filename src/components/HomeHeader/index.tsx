ed90a30603cc23c7d168394f0dea3214c2b3d0f9
Natanaelvich <taelima1997@gmail.com>
2023-05-26 17:45:58 -0300

    add user data to header and add logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,25 +5,33 @@ import { Container, Greeting, Message, Name, Picture } from "./styles";
 
 import theme from "../../theme";
 import { Power } from "phosphor-react-native";
+import { useApp, useUser } from "@realm/react";
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
   const insets = useSafeAreaInsets();
 
   const paddingTop = insets.top + 32;
 
+  function handleLogOut() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container style={{ paddingTop }}>
       <Picture
-        source={{ uri: "https://github.com/natanaelvich.png" }}
+        source={{ uri: user?.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
       />
       <Greeting>
         <Message>Olá</Message>
 
-        <Name>Natanael</Name>
+        <Name>{user?.profile.name}</Name>
       </Greeting>
 
-      <TouchableOpacity activeOpacity={0.7}>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
