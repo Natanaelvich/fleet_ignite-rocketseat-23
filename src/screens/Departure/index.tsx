061dbf1cee462076e63db6440a2d99f8a9187f8d
Natanaelvich <taelima1997@gmail.com>
2023-05-27 08:11:20 -0300

    add state form and validation plate

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { ScrollView, TextInput, View, findNodeHandle } from "react-native";
+import { useRef, useState } from "react";
+import { Alert, TextInput, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { Button } from "../../components/Button";
@@ -8,11 +8,48 @@ import { Container, Content } from "./styles";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Header } from "../../components/Header";
+import { useNavigation } from "@react-navigation/native";
+import { licensePlateValidate } from "../../utils/licensePlateValidate";
 
 export function Departure() {
+  const { goBack } = useNavigation();
+
+  const descriptionRef = useRef<TextInput>(null);
+  const licensePlateRef = useRef<TextInput>(null);
+
   const [description, setDescription] = useState("");
   const [licensePlate, setLicensePlate] = useState("");
   const [isRegistering, setIsResgistering] = useState(false);
+
+  function handleDepartureRegister() {
+    try {
+      if (!licensePlateValidate(licensePlate)) {
+        licensePlateRef.current?.focus();
+        return Alert.alert(
+          "Placa inválida",
+          "A placa é inválida. Por favor, informa a placa correta."
+        );
+      }
+
+      if (description.trim().length === 0) {
+        descriptionRef.current?.focus();
+        return Alert.alert(
+          "Finalidade",
+          "Por favor, informe a finalidade da utilização do veículo"
+        );
+      }
+
+      setIsResgistering(false);
+
+      Alert.alert("Saída", "Saída do veículo registrada com sucesso.");
+
+      goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não possível registrar a saída do veículo.");
+      setIsResgistering(false);
+    }
+  }
 
   return (
     <Container>
@@ -22,21 +59,31 @@ export function Departure() {
         <View>
           <Content>
             <LicensePlateInput
+              ref={licensePlateRef}
               label="Placa do veículo"
               placeholder="BRA1234"
               returnKeyType="next"
               onChangeText={setLicensePlate}
+              onSubmitEditing={() => {
+                descriptionRef.current?.focus();
+              }}
             />
 
             <TextAreaInput
+              ref={descriptionRef}
               label="Finalizade"
               placeholder="Vou utilizar o veículo para..."
               returnKeyType="send"
               blurOnSubmit
               onChangeText={setDescription}
+              onSubmitEditing={handleDepartureRegister}
             />
 
-            <Button title="Registar Saída" isLoading={isRegistering} />
+            <Button
+              title="Registar Saída"
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
           </Content>
         </View>
       </KeyboardAwareScrollView>
