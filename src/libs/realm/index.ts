5ce832dfc2aef8bee33a78fc92ab8a83f41780a9
Natanaelvich <taelima1997@gmail.com>
2023-05-27 22:51:53 -0300

    add sync config

--- src/libs/realm/index.ts
+++ src/libs/realm/index.ts
@@ -1,6 +1,17 @@
 import { createRealmContext } from '@realm/react'
 
 import { Historic } from './schemas/Historic'
+import { OpenRealmBehaviorConfiguration, OpenRealmBehaviorType } from 'realm'
+
+const realmAccessBehavior: OpenRealmBehaviorConfiguration = {
+  type: OpenRealmBehaviorType.OpenImmediately,
+}
+
+export const syncConfig: any = {
+  flexible: true,
+  newRealmFileBehavior: realmAccessBehavior,
+  existingRealmFileBehavior: realmAccessBehavior,
+}
 
 export const { RealmProvider, useRealm, useQuery, useObject } =
   createRealmContext({
