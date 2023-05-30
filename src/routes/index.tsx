2cbff73ce6ce959e445cea19aa2415d55d483b26
Natanaelvich <taelima1997@gmail.com>
2023-05-29 22:13:32 -0300

    add config toast

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native'
+import Toast from 'react-native-toast-message'
 
 import { AppRoutes } from './app.routes'
+import { TopMessage } from '../components/TopMessage'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function Routes() {
+  const insets = useSafeAreaInsets()
+
   return (
     <NavigationContainer>
       <AppRoutes />
+
+      <Toast
+        config={{
+          info: ({ text1 }) => <TopMessage title={String(text1)} />,
+        }}
+        topOffset={insets.top}
+      />
     </NavigationContainer>
   )
 }
