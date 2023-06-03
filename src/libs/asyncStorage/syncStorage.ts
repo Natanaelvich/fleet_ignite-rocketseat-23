66688f7e476ca5dc6d327b36e088dfaa8b6875c0
Natanaelvich <taelima1997@gmail.com>
2023-06-02 22:23:42 -0300

    update with mmkv

--- src/libs/asyncStorage/syncStorage.ts
+++ src/libs/asyncStorage/syncStorage.ts
@@ -1,17 +1,19 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { MMKV } from 'react-native-mmkv'
+
+export const storage = new MMKV()
 
 const STORAGE_ASSYNC_KEY = '@ignitefleet:last_sync'
 
 export async function saveLastSyncTimestamp() {
   const timestamp = new Date().getTime()
 
-  await AsyncStorage.setItem(STORAGE_ASSYNC_KEY, timestamp.toString())
+  storage.set(STORAGE_ASSYNC_KEY, timestamp.toString())
 
   return timestamp
 }
 
 export async function getLastAsyncTimestamp() {
-  const timestamp = await AsyncStorage.getItem(STORAGE_ASSYNC_KEY)
+  const timestamp = storage.getString(STORAGE_ASSYNC_KEY)
 
   return Number(timestamp)
 }
