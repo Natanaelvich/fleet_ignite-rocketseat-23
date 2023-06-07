08cf7345576f7d869c2084a50cfb7a5c3dcfaa62
Natanaelvich <taelima1997@gmail.com>
2023-06-06 22:47:05 -0300

    remove version development android

--- app.config.js
+++ app.config.js
@@ -1,36 +1,30 @@
-const IS_DEV = process.env.APP_VARIANT === 'development'
-
 export default {
-  name: IS_DEV ? 'Fleet Dev' : 'Fleet',
+  name: 'Fleet',
   slug: 'fleet',
   version: '1.0.0',
   orientation: 'portrait',
-  icon: IS_DEV ? './assets/icon-dev.png' : './assets/icon.png',
+  icon: './assets/icon.png',
   userInterfaceStyle: 'light',
-  scheme: IS_DEV ? 'com.natanaelvich.fleetdev' : 'com.natanaelvich.fleet',
+  scheme: 'com.natanaelvich.fleet',
   splash: {
-    image: IS_DEV ? './assets/splash-dev.png' : './assets/splash.png',
+    image: './assets/splash.png',
     resizeMode: 'cover',
     backgroundColor: '#202024',
   },
   assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: IS_DEV
-      ? 'com.natanaelvich.fleet-dev'
-      : 'com.natanaelvich.fleet',
+    bundleIdentifier: 'com.natanaelvich.fleet',
     config: {
       googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
     },
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: IS_DEV
-        ? './assets/adaptive-icon-dev.png'
-        : './assets/adaptive-icon.png',
+      foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#202024',
     },
-    package: IS_DEV ? 'com.natanaelvich.fleetdev' : 'com.natanaelvich.fleet',
+    package: 'com.natanaelvich.fleet',
     config: {
       googleMaps: {
         apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
@@ -38,7 +32,7 @@ export default {
     },
   },
   web: {
-    favicon: IS_DEV ? './assets/favicon-dev.png' : './assets/favicon.png',
+    favicon: './assets/favicon.png',
   },
   extra: {
     eas: {
