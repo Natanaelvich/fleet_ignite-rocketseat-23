2ece4f493e641a72e3e04308a2326d6a4f0ae9fc
Natanaelvich <taelima1997@gmail.com>
2023-06-09 20:09:30 -0300

    update schemme to support coords

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -4,6 +4,16 @@ import { Realm } from '@realm/react'
 type GenerateProps = {
   user_id: string
   description: string
+  coordinates: {
+    initial: {
+      latitude: number
+      longitude: number
+    }
+    final: {
+      latitude: number
+      longitude: number
+    }
+  }
   license_plate: string
 }
 
@@ -13,15 +23,32 @@ export class Historic extends Realm.Object<Historic> {
   license_plate!: string
   description!: string
   status!: string
+  coordinates!: {
+    initial: {
+      latitude: number
+      longitude: number
+    }
+    final: {
+      latitude: number
+      longitude: number
+    }
+  }
+
   created_at!: Date
   updated_at!: Date
 
-  static generate({ user_id, description, license_plate }: GenerateProps) {
+  static generate({
+    user_id,
+    description,
+    license_plate,
+    coordinates,
+  }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
       user_id,
       description,
       license_plate,
+      coordinates,
       status: 'departure',
       created_at: new Date(),
       updated_at: new Date(),
@@ -40,6 +67,25 @@ export class Historic extends Realm.Object<Historic> {
       },
       license_plate: 'string',
       description: 'string',
+      coordinates: {
+        type: 'object',
+        properties: {
+          initial: {
+            type: 'object',
+            properties: {
+              latitude: 'double',
+              longitude: 'double',
+            },
+          },
+          final: {
+            type: 'object',
+            properties: {
+              latitude: 'double',
+              longitude: 'double',
+            },
+          },
+        },
+      },
       status: 'string',
       created_at: 'date',
       updated_at: 'date',
