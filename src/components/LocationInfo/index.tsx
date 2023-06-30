7a669515170ca9de4eda21959366330926790519
Natanaelvich <taelima1997@gmail.com>
2023-06-30 20:35:15 -0300

    add comp LocationInfo

---
+++ src/components/LocationInfo/index.tsx
@@ -0,0 +1,25 @@
+import { IconBox, IconBoxProps } from '../IconBox'
+import { Container, Description, Info, Label } from './styles'
+
+export type LocationInfoProps = {
+  label: string
+  description: string
+}
+
+type Props = LocationInfoProps & {
+  icon: IconBoxProps
+}
+
+export function LocationInfo({ icon, label, description }: Props) {
+  return (
+    <Container>
+      <IconBox icon={icon} />
+
+      <Info>
+        <Label numberOfLines={1}>{label}</Label>
+
+        <Description numberOfLines={1}>{description}</Description>
+      </Info>
+    </Container>
+  )
+}
