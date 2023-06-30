97e8af7f65e3ed8e84e571bfeb0ecf6b54f8af58
Natanaelvich <taelima1997@gmail.com>
2023-06-30 20:35:46 -0300

    add message comp

--- src/screens/Departure/styles.ts
+++ src/screens/Departure/styles.ts
@@ -11,3 +11,10 @@ export const Content = styled.View`
   padding: 32px;
   margin-top: 16px;
 `
+
+export const Message = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  text-align: center;
+  margin: 24px;
+`
