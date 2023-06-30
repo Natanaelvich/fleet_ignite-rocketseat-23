53b93f64f409216ea8a817a90412013a52b655c0
Natanaelvich <taelima1997@gmail.com>
2023-06-30 20:35:30 -0300

    add utils getAddressLocation

---
+++ src/utils/getAddressLocation.ts
@@ -0,0 +1,14 @@
+import { LocationObjectCoords, reverseGeocodeAsync } from 'expo-location'
+
+export async function getAddressLocation({
+  latitude,
+  longitude,
+}: LocationObjectCoords) {
+  try {
+    const addressResponse = await reverseGeocodeAsync({ latitude, longitude })
+
+    return addressResponse[0]?.street
+  } catch (error) {
+    console.log(error)
+  }
+}
