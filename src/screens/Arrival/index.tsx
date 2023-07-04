b3f76542fe4724e5a223ed72d2976a9159a42dc6
Natanaelvich <taelima1997@gmail.com>
2023-07-03 22:13:35 -0300

    update with new component map

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -8,8 +8,7 @@ import { BSON } from 'realm'
 import { Button } from '../../components/Button'
 import { ButtonIcon } from '../../components/ButtonIcon'
 import { Header } from '../../components/Header'
-import HistoricMap from '../../components/HistoricMap'
-import HistoricMapDirections from '../../components/HistoricMapDirections'
+import { Map } from '../../components/Map'
 import { useObject, useRealm } from '../../libs/realm'
 import { Historic } from '../../libs/realm/schemas/Historic'
 import { getLastAsyncTimestamp } from '../../libs/storage/mmkv'
@@ -113,35 +112,31 @@ export function Arrival() {
     })()
   }, [])
 
-  const handlePressMap = (latitude: number, longitude: number) => {
-    setLocation({
-      coords: {
-        latitude,
-        longitude,
-      },
-    })
-  }
-
   return (
     <Container>
       <Header title={title} />
       <Content>
         {historic?.status === 'departure' ? (
-          <HistoricMap
-            latitude={location?.coords.latitude}
-            longitude={location?.coords.longitude}
-            onChange={handlePressMap}
+          <Map
+            coordinates={[
+              {
+                latitude: location?.coords.latitude ?? 0,
+                longitude: location?.coords.longitude ?? 0,
+              },
+            ]}
           />
         ) : (
-          <HistoricMapDirections
-            origin={{
-              latitude: historic?.initial_latitude ?? 0,
-              longitude: historic?.initial_longitude ?? 0,
-            }}
-            destination={{
-              latitude: historic?.final_latitude ?? 0,
-              longitude: historic?.final_longitude ?? 0,
-            }}
+          <Map
+            coordinates={[
+              {
+                latitude: historic?.initial_latitude ?? 0,
+                longitude: historic?.initial_longitude ?? 0,
+              },
+              {
+                latitude: historic?.final_latitude ?? 0,
+                longitude: historic?.final_longitude ?? 0,
+              },
+            ]}
           />
         )}
 
