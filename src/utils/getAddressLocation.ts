cf8d3026c2e6980c8f5e16a914b288f06fce2846
Natanaelvich <taelima1997@gmail.com>
2023-07-14 22:51:04 -0300

    update type

--- src/utils/getAddressLocation.ts
+++ src/utils/getAddressLocation.ts
@@ -1,9 +1,14 @@
-import { LocationObjectCoords, reverseGeocodeAsync } from 'expo-location'
+import { reverseGeocodeAsync } from 'expo-location'
+
+type GetAddressLocation = {
+  latitude: number
+  longitude: number
+}
 
 export async function getAddressLocation({
   latitude,
   longitude,
-}: LocationObjectCoords) {
+}: GetAddressLocation): Promise<string | undefined> {
   try {
     const addressResponse = await reverseGeocodeAsync({ latitude, longitude })
 
