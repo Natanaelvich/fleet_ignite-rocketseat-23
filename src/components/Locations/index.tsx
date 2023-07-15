fa5e0f3ed5c8c97705ffa4d5465fae76f4a1e009
Natanaelvich <taelima1997@gmail.com>
2023-07-14 22:48:53 -0300

    add locations component

---
+++ src/components/Locations/index.tsx
@@ -0,0 +1,33 @@
+import { Car, FlagCheckered } from 'phosphor-react-native'
+
+import { LocationInfo, LocationInfoProps } from '../LocationInfo'
+import { Container, Line } from './styles'
+
+type Props = {
+  departure: LocationInfoProps
+  arrival?: LocationInfoProps | null
+}
+
+export function Locations({ arrival = null, departure }: Props) {
+  return (
+    <Container>
+      <LocationInfo
+        icon={Car}
+        label={departure.label}
+        description={departure.description}
+      />
+
+      {arrival && (
+        <>
+          <Line />
+
+          <LocationInfo
+            icon={FlagCheckered}
+            label={arrival.label}
+            description={arrival.description}
+          />
+        </>
+      )}
+    </Container>
+  )
+}
