b60aabcf1c637aee69e701deb02102fa384fe806
Natanaelvich <taelima1997@gmail.com>
2023-07-14 21:06:33 -0300

    add location storage lib

---
+++ src/libs/asyncStorage/locationStorage.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const STORAGE_KEY = '@ignitefleet:location'
+
+type LocationProps = {
+  latitude: number
+  longitude: number
+  timestamp: number
+}
+
+export async function getStorageLocations() {
+  const storage = await AsyncStorage.getItem(STORAGE_KEY)
+
+  const response = storage ? JSON.parse(storage) : []
+
+  return response
+}
+
+export async function saveStorageLocation(newLocation: LocationProps) {
+  const storage = await getStorageLocations()
+
+  storage.push(newLocation)
+
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
+}
+
+export async function removeStorageLocations() {
+  await AsyncStorage.removeItem(STORAGE_KEY)
+}
